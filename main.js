b03d946464a18fde1bda403cfe850bbc0a2a2daa
Student <student@RU-109.local>
2015-09-14 14:06:16 -0600

    1st

---
+++ main.js
@@ -0,0 +1,11 @@
+// var animals = ["rat", "cat", "butterfly", "marmot", "ocelot"];
+
+// for(var i=0; i<(animals.length-1); i++) {
+// 	console.log(animals[i]);
+// }
+
+var animals = ["rat", "cat", "butterfly", "marmot", "ocelot"];
+
+for(var i=0; i<animals.length; i++) {
+	console.log(animals[i]);
+}
